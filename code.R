a9fdf40550cbc09724ecd022ba99aade6f7f9c31
Italo sayan <italosayan@gmail.com>
2017-10-20 15:45:59 -0400

    Update code.R

--- code.R
+++ code.R
@@ -1,1 +1,8 @@
-
+#function that filters numberic variables
+getNumericColumns<-function(t){
+    tn = sapply(t,function(x){is.numeric(x)})
+    return(names(tn)[which(tn)])
+}
+library(corrplot)
+#correlation of numeric variables
+corrplot(cor(dataLC[getNumericColumns(dataLC)],use="na.or.complete"))
